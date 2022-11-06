cb888fc3cb171361f85a70b929d5f22a164f3b79
Burak Kuru <burak.kuru@abkholding.com>
2022-11-06 22:23:16 +0300

    lesson5. buttons and functions

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+
+    const handleClick = (e) =>{
+        console.log('clicked',e)
+    }
+    const handleClickAgain = (name,e) =>{
+        console.log('clicked by '+name, e.target)
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e)=> handleClickAgain('burak',e)}>Click me again</button>
         </div>
      );
 }
