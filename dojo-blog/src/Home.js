74c73d06c5414995fe8e01d880b90dffa612384d
Burak Kuru <burak.kuru@abkholding.com>
2022-11-14 21:26:54 +0300

    L-11 fetch from json server and set blogs with that data

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,24 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs,setBlogs] = useState([
+    const [blogs,setBlogs] = useState(null)
 
-        { title: ' A Clockwork Orange', body: 'lorem ipsum...', author: 'Anthony Burgess', id: 1 },
-        { title: '1984', body: 'lorem ipsum...', author: 'Gerorge Orwell', id: 2 },
-        { title: 'Kürk Mantolu Madonna ', body: 'lorem ipsum...', author: 'Sabahattin Ali', id: 3 }
-    ])
+    useEffect(() => {
 
-    const handleDelete = (id) => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        });
 
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    },[]);
 
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="All blogs!" handleDelete ={handleDelete}/>
+           {blogs && <BlogList blogs={blogs} title="All blogs!"/>}
 
         </div>
      );
