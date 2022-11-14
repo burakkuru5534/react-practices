9ce37853c36f92dda59285afcdd6e0fd7a74afce
Burak Kuru <burak.kuru@abkholding.com>
2022-11-14 20:33:21 +0300

    function as a props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -10,9 +10,15 @@ const Home = () => {
         { title: 'Kürk Mantolu Madonna ', body: 'lorem ipsum...', author: 'Sabahattin Ali', id: 3 }
     ])
 
+    const handleDelete = (id) => {
+
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="All blogs!"/>
+           <BlogList blogs={blogs} title="All blogs!" handleDelete ={handleDelete}/>
 
         </div>
      );
