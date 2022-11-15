cd581eeb2014e47b50446fe9f0dcfaac295521a7
Burak Kuru <burak.kuru@abkholding.com>
2022-11-15 20:21:00 +0300

    L-13. loading message and usage of settimeout

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,23 +4,27 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs,setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
-
+        setTimeout(() => {
+            
         fetch('http://localhost:8000/blogs')
         .then(res => {
             return res.json();
         })
         .then(data => {
             setBlogs(data);
+            setIsPending(false);
         });
+        }, 1000);
 
     },[]);
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
            {blogs && <BlogList blogs={blogs} title="All blogs!"/>}
-
         </div>
      );
 }
