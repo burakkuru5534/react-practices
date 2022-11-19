3579e6acd582a7ce24c2ca7abf2cce1cfc70f156
Burak Kuru <burak.kuru@abkholding.com>
2022-11-19 14:51:11 +0300

    L-24. delete and reroute it to home page

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -6,6 +6,17 @@ const BlogDetails = () => {
 const { id } = useParams();
 
 const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id)
+const history = useHistory();
+
+const handleDelete = () => {
+
+  fetch('http://localhost:8000/blogs/' + id, {
+    method: 'DELETE'
+  }).then(() => {
+    history.push('/');
+  });
+
+}
 
     return ( 
         <div className="blog-details">
@@ -17,6 +28,7 @@ const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' +
               <h2>  { blog.title }</h2>
               <p>Written by {blog.author}</p>
               <div>{ blog.body }</div>
+              <button onClick={handleDelete}>Delete</button>
             </article>
         ) }
 
